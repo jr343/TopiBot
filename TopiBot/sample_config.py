41e7a7bf75244fa7379f65bd21d9cfba225e9203
Jay Chaudhary <82112442+jr343@users.noreply.github.com>
2022-02-03 08:27:11 +0530

    Update sample_config.py

--- TopiBot/sample_config.py
+++ TopiBot/sample_config.py
@@ -17,14 +17,14 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 797768146  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "HEROGAMERS1" 
-    SUPPORT_CHAT = "AwesomeSupport"  # Your own group for support, do not add the @
+    OWNER_ID = 2111011493  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "chaudhary_Anjana" 
+    SUPPORT_CHAT = "the_godzilla_network_support"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001392103597
+        -1001612532249
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001392103597
+        -1001612532249
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
